053d1e163d8232de34567698d34d57cc329d2567
rabby782islam-design <rabby782islam@gmail.com>
2026-03-08 09:43:53 +0600

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T03:43:34.074Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-08T03:44:01.281Z
+Commit: Fix memory leak in event handlers
