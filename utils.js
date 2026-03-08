75c66074a5f04c2f7988347a2d527fe760205bb4
rabby782islam-design <rabby782islam@gmail.com>
2026-03-08 09:45:09 +0600

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T03:43:56.891Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-08T03:45:18.042Z
+Commit: Fix broken links in documentation
